601bfd3ce7eb302b5ae9b347d8ecb6c77784e8e5
Jesse Alama <jesse.alama@gmail.com>
2012-09-26 15:58:58 +0200

    Use suitable Unicode to represent the connectives, etc.

--- drawing-board/leibniz-proof.lisp
+++ drawing-board/leibniz-proof.lisp
@@ -1,15 +1,22 @@
 ;; Let's try to think of a nice way to represent the proof of Theorem
 ;; 4 of "A (Leibnizian) theory of concepts", p. 21.
 
+(reserve x object)
+(reserve y object)
+(reserve F property)
+(reserve G property)
+(reserve H property)
+(reserve P property)
+
 (definition
     (let ((x obect)
 	  (y object)))
-    (real-sum x y)
+    (⊕ x y)
   :means
-  (and (concept it)
-       (forall ((F property))
-	       (iff [z F] (or [x F]
-			      [z F]))))
+  (∧ (concept it)
+     (∀ F (⟷ [z F]
+	       (∨ [x F]
+		  [z F]))))
   :existence
   ;; missing proof
   :uniqueness
@@ -20,40 +27,34 @@
     (let ((G property)))
     (concept-of G)
   :means
-  (and (concept it)
-       (forall ((F property))
-	       (iff [x F]
-		    (implies G F))))
+  (∧ (concept it)
+       (∀ F (⟷ [x F]
+	         (⊂ G F))))
   :existence
   ;; missing proof
   :uniqueness
   ;; missing proof
   )
 
-(reserve G property)
-(reserve H property)
-
 (theorem
- (= (real-sum (concept-of G)
-	      (concept-of H))
-    (the ((x object))
-      (and (concept x)
-	   (forall ((F property))
-		   (iff [x F]
-			(or (implies G F)
-			    (implies H F)))))))
+ (= (⊕ (concept-of G)
+       (concept-of H))
+    (℩ x
+       (∧ (concept x)
+	  (∀ F (⟷ [x F]
+		    (∨ (⊂ G F)
+		       (⊂ H F)))))))
  (proof
   (set description
-       (the ((x object))
-	 (and (concept x)
-	      (forall ((F property))
-		      (iff [x F]
-			   (or (implies G F)
-			       (implies H F)))))))
+       (℩ (x object)
+	  (∧ (concept x)
+	     (∀ F (⟷ [x F]
+		     (∨ (⊂ G F)
+			(⊂ H F)))))))
   (set cG (concept-of G))
   (set cH (concept-of H))
-  (set sum (real-sum cH cH))
-  (:A (forall ((P property)) [sum P]))
-  (:B (forall ((P property)) [sum P]))
-  (:C (A-equal sum description) :by :A :B :def-A-equal)
+  (set sum (⊕ cH cH))
+  (:A (∀ P [sum P]))
+  (:B (∀ P [sum P]))
+  (:C (A= sum description) :by :A :B :def-A-equal)
   (hence thesis :C :def-equality)))
