f234b21a31281c44cf92157ede943fd8c3fb4937
Jesse Alama <jesse.alama@gmail.com>
2012-09-26 16:41:42 +0200

    Simplify schemes.  Use Unicode.

--- drawing-board/principles.lisp
+++ drawing-board/principles.lisp
@@ -1,60 +1,37 @@
+(reserve x object)
+(reserve y object)
+(reserve F property)
+
 (axiom
  :name "Principle 1"
- (forall ((x object))
-	 (implies (ordinary x)
-		  (necessarily (not (exists ((F property))
-					    (F x)))))))
+ (⟶ (E! x) (□ (∄ F (F x)))))
 
 (axiom
  :name "Definition of E-equal"
- (forall ((x object)
-	  (y object))
-	 (iff (E-equal x y)
-	      (and (ordinary x)
-		   (ordinary y)
-		   (necessarily (forall ((F property))
-					(iff (F x)
-					     (F y))))))))
+ (⟷ (E≈ x y)
+    (∧ (E! x)
+	 (E! y)
+	 (□ (∀ F (⟷ (F x) (F y)))))))
 
 (axiom-scheme
  :name "Principle 3"
- (lambda (phi)
-   (exists ((x object))
-	   (and (abstract x)
-		(forall ((F property))
-			(iff (x F)
-			     phi)))))
+ (∃ ((?x object))
+    (∧ (A! ?x)
+       (∀ F
+	  (⟷ (?x F) ?φ))))
  :proviso
- (lambda (purported-instance)
-   (let ((schematic-formula (exists ((?x object))
-				    (and (abstract ?x)
-					 (forall ((?F property))
-						 (iff (?x ?F)
-						      ?phi))))))
-     (let ((substitution (unify purported-instance schematic-formula)))
-       (when (not (eq substitution :fail))
-	 (let ((x (value-in-substitution ?x substitution))
-	       (F (value-in-substitution ?F substitution))
-	       (phi (value-in-substitution ?phi)))
-	   (and (not (free-in x phi))
-		(subsetp (free-variables phi)
-			 (list ?F)))))))))
+ (and (not (free-in ?x ?φ))
+      (subsetp (free-variables ?φ) (list ?x))))
 
 (axiom
  :name "Principle 4"
- (forall ((x object)
-	  (y object))
-	 (iff (A-equal x y)
-	      (and (abstract x)
-		   (abstract y)
-		   (necessarily (forall ((F property))
-					(iff (x F)
-					     (y F))))))))
+ (⟷ (A≈ x y)
+    (∧ (A! x)
+       (A! y)
+       (□ (∀ F (⟷ (x F) (y F)))))))
 
 (axiom
  :name "Principle 5"
- (forall ((x object)
-	  (y object))
-	 (iff (≈ x y)
-	      (or (A≈ x y)
-		  (E≈ x y)))))
+ (⟷ (≈ x y)
+    (∨ (A≈ x y)
+       (E≈ x y))))
